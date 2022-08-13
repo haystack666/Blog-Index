9e81285093e63a83aba69308777e2d5c48c90ac5
Haystack <56021097+haystack666@users.noreply.github.com>
2022-08-14 00:34:23 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ const GITHUB = ""; // 右上角 Github 图标链接，留空不显示
 const BACKGROUND_IMG_URL = "https://api.dujin.org/bing/1920.php"; // 背景图片url，默认为必应每日图片（第三方接口）
 
 const SORT_DATA = [
-  { sortId: 1, title: "网址导航" },
+  { sortId: 1, title: "常用链接" },
   { sortId: 2, title: "个人网站" }
 ]; // 网站分类列表，请按照API.md中'/getSort'接口中返回的data格式填写
 
@@ -21,19 +21,99 @@ const PAGES_DATA = [
   {
     pageId: 1,
     sortId: 1,
-    title: "主标题",
-    subtitle: "副标题",
-    url: "http://www.YourWebPage.com/",
+    title: "百度",
+    subtitle: "国内最大的搜索引擎",
+    url: "https://www.baidu.com/",
+    icon: ""
+  },
+  {
+    pageId: 2,
+    sortId: 1,
+    title: "Goole",
+    subtitle: "全球最大的搜索引擎",
+    url: "https://www.google.com/",
+    icon: ""
+  },
+  {
+    pageId: 3,
+    sortId: 1,
+    title: "IT之家",
+    subtitle: "爱科技，爱这里",
+    url: "https://www.ithome.com/",
+    icon: ""
+  },
+  {
+    pageId: 4,
+    sortId: 1,
+    title: "知乎",
+    subtitle: "有问题，就会有答案",
+    url: "https://www.zhihu.com/",
+    icon: ""
+  },
+  {
+    pageId: 5,
+    sortId: 1,
+    title: "掘金",
+    subtitle: "面向全球中文开发者的技术内容分享与交流平台",
+    url: "https://www.juejin.cn/",
+    icon: ""
+  },
+  {
+    pageId: 6,
+    sortId: 1,
+    title: "CSDN",
+    subtitle: "全球知名中文IT技术交流平台",
+    url: "https://www.csdn.net/",
+    icon: ""
+  },
+  {
+    pageId: 7,
+    sortId: 1,
+    title: "持续更新",
+    subtitle: "",
+    url: "https://imhay.icu/",
+    icon: ""
+  },
+  {
+    pageId: 1,
+    sortId: 2,
+    title: "Haystack的主页",
+    subtitle: "欢迎来到Haystack的主页",
+    url: "https://imhay.icu/",
     icon: ""
   },
   {
     pageId: 2,
     sortId: 2,
-    title: "主标题",
-    subtitle: "副标题",
-    url: "http://www.YourWebPage.xyz/",
+    title: "Haystack的小屋",
+    subtitle: "欢迎来到Haystack的小屋",
+    url: "https://blog.imhay.icu/",
     icon: ""
-  }
+  },
+  {
+    pageId: 3,
+    sortId: 2,
+    title: "Haystack的云盘",
+    subtitle: "欢迎来到Haystack的云盘",
+    url: "https://drive.imhay.icu/",
+    icon: ""
+  },
+  {
+    pageId: 4,
+    sortId: 2,
+    title: "Haystack的音乐",
+    subtitle: "欢迎来到Haystack的音乐",
+    url: "https://music.imhay.icu/",
+    icon: ""
+  },
+  {
+    pageId: 4,
+    sortId: 2,
+    title: "Haystack的站点检测",
+    subtitle: "欢迎来到Haystack的站点检测",
+    url: "https://status.imhay.icu/",
+    icon: ""
+  },
 ]; // 网站分类列表，请按照API.md中'/getPages'接口中返回的data格式填写
 
 const SLOGAN = [
@@ -48,7 +128,7 @@ const SLOGAN = [
   "马车越空，晃荡越响"
 ]; // 在Logo下方显示的标题
 
-const FOOTER_INFO = true; // 是否显示抽屉右下角的文字信息
+const FOOTER_INFO = false; // 是否显示抽屉右下角的文字信息
 
 export default {
   SERVE,
